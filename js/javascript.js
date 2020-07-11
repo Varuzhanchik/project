6221db0fbed4d8bf65e7d88cde4baef5f8d63231
Varuzhan <example@gmail.com>
2020-07-11 18:47:48 +0300

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,17 @@
+//document.getElementById("priv").onclick=function () {
+    alert("Привет");
+    console.log(1);
+    let nameN = 1;
+    const leftBorder = 1;
+    console.log(nameN);
+    
+
+    var obj = {
+        name: 'Varuzhan',
+        age: 27,
+
+
+    };
+    console.log(obj.age);
+
+//}
