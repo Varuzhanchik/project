7e6488ab7505f98f06290c1552933afc3095c611
Varuzhan <varuzhan.sarkisov@mail.ru>
2020-12-17 00:32:17 +0300

    form

--- Food/dist/js/script.js
+++ Food/dist/js/script.js
@@ -133,5 +133,48 @@ window.addEventListener('DOMContentLoaded', () =>{
     //card.priceTotal(this.price);
 
 
+    //form
+    let forms = document.querySelectorAll('form');
+
+    const message={
+        loading: 'Загрузка',
+        success: 'Спасибо',
+        failure: 'что то пошло нетак'
+    };
+
+    forms.forEach(item =>{
+        postData(item);
+    });
+    
+    function postData(form) {
+        form.addEventListener('submit',(e)=>{
+            e.preventDefault();
+
+            const statusMessage = document.createElement("div");
+            statusMessage.classList.add('status');
+            statusMessage.textContent= message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+
+            //request.setRequestHeader('Content-type', 'multipart/fotm-data');
+            const formData = new FormData(form);
+            request.send(formData);
+
+            request.addEventListener('load', ()=>{
+                if (request.status===200){
+                    console.log(request.response);
+                    statusMessage.textContent= message.success;
+                } else{
+                    statusMessage.textContent= message.failure;
+                }
+            });
+
+
+        });
+        
+    }
+
 
 });
